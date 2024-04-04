ebd76398c85c1b0215569b0ffe796fb4ff925f7c
Justin Han <gksksla7140@gmail.com>
2024-04-03 22:49:22 -0700

    Add game timer and restart functionality

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,40 +1,95 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PlayerInput from "@/components/game/PlayerInput";
 import GameText from "@/components/game/GameText";
 
-const testPhrase =
-  "Hello this is a test phrase to type out in the game component. But it's not a game yet, just a test.";
+const testPhrase = "Hello testing this world hello my ages in 25";
+const misMatchLimit = 15;
 
 export default function Game() {
   const [typedText, setTypedText] = useState("");
   const [matchingLen, setMatchingLen] = useState(0);
   const [startIdx, setStartIdx] = useState(0);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameEnded, setGameEnded] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (gameStarted && !gameEnded) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [gameStarted, gameEnded]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
 
+    if (startIdx + input.length > testPhrase.length) {
+      return;
+    }
+
+    if (matchingLen + input.length > misMatchLimit) {
+      return;
+    }
+
+    if (!gameStarted) {
+      console.log("Game started!");
+      setGameStarted(true);
+    }
+
+    const remainingPhrase = testPhrase.slice(startIdx + input.length);
+
+    if (input.endsWith(" ") && testPhrase.slice(startIdx).startsWith(input)) {
+      handleWordCompleted(input.length, remainingPhrase);
+      return;
+    }
+    if (
+      input.length + startIdx === testPhrase.length &&
+      input === testPhrase.slice(startIdx)
+    ) {
+      setGameEnded(true);
+    }
+    handleWordInProgress(input, input.length);
+  };
+
+  const handleWordCompleted = (inputLen: number, remainingPhrase: string) => {
+    setStartIdx(startIdx + inputLen);
+    setTypedText("");
+    setMatchingLen(0);
+    if (remainingPhrase.trim().length === 0) {
+      console.log("Game ended!");
+      setGameEnded(true);
+    }
+  };
+
+  const handleWordInProgress = (input: string, inputLen: number) => {
     let newMatchingLen = 0;
     while (
-      newMatchingLen < input.length &&
+      newMatchingLen < inputLen &&
       newMatchingLen < testPhrase.length &&
       input[newMatchingLen] === testPhrase[startIdx + newMatchingLen]
     ) {
       newMatchingLen++;
     }
 
-    if (input.endsWith(" ") && testPhrase.slice(startIdx).startsWith(input)) {
-      setStartIdx(startIdx + input.length);
-      setTypedText("");
-      setMatchingLen(0);
-    } else if (matchingLen + 15 > input.length) {
-      setMatchingLen(newMatchingLen);
-      setTypedText(input);
-    }
+    setMatchingLen(newMatchingLen);
+    setTypedText(input);
+  };
 
-    if (testPhrase.slice(startIdx + input.length).trim().length === 0) {
-      // implement game end
-    }
+  const handleReset = () => {
+    setTypedText("");
+    setMatchingLen(0);
+    setStartIdx(0);
+    setGameStarted(false);
+    setGameEnded(false);
+    setElapsedTime(0);
   };
 
   return (
@@ -45,9 +100,19 @@ export default function Game() {
           typedText={typedText}
           matchingLen={matchingLen}
           startIdx={startIdx}
+          gameStarted={gameStarted}
+          gameEnded={gameEnded}
+          elapsedTime={elapsedTime}
         />
-        <PlayerInput value={typedText} onChange={handleChange} />
+        <h1>{elapsedTime}</h1>
       </div>
+      {!gameEnded && <PlayerInput value={typedText} onChange={handleChange} />}
+      {gameEnded && (
+        <div className="text-black">
+          <p>Game ended! Time elapsed: {elapsedTime} seconds</p>
+          <button onClick={handleReset}>Restart</button>
+        </div>
+      )}
     </div>
   );
 }
